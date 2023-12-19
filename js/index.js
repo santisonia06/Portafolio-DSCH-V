876befe187767d59d46772bfc45b31e556b6ea19
santisonia06 <santisonia06@gmail.com>
2023-12-18 23:49:14 -0500

    Agrego diseño para mobile

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
+//REFRESH
 function refresh() {
 	window.location.reload()
 	let R = document.getElementById("recargar");
 	 R.style.display="block";
 	 
     }
+	function compruebaAceptaCookies() {
+		if(localStorage.aceptaCookies != 'true'){
+		  cajacookies.style.display = 'block';
+		}
+		
+	  
+	  }
+	  
+//COKIE
+function compruebaAceptaCookies() {
+	if(localStorage.aceptaCookies != 'true'){
+	  cajacookies.style.display = 'block';
+	}
+  }
+	  function aceptarCookies() {
+	   localStorage.aceptaCookies = 'true';
+		cajacookies.style.display = 'none';
+	  
+	  }
+
+	  $(document).ready(function () {
+		compruebaAceptaCookies();
+		 
+	  });
+	  
