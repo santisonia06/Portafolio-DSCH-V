73e6573bce15707114c872967ecd3584d35480ca
santisonia06 <santisonia06@gmail.com>
2023-12-20 10:44:57 -0500

    cambios en la estructura para login

--- js/index.js
+++ js/index.js
@@ -12,7 +12,60 @@ function refresh() {
 		
 	  
 	  }
-	  
+	  //BUSCAR CODIGO
+	  	
+
+	document.getElementById("passwordForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    var password = document.getElementById("password").value;
+	var zona6 = document.getElementById("zona6");
+	var zona5 = document.getElementById("zona5");
+	var zona4 = document.getElementById("zona4");
+	var zona3 = document.getElementById("zona3");
+	var zona2 = document.getElementById("zona2");
+	var zona1 = document.getElementById("zona1");
+
+    if (password === "contraseña") {
+        document.getElementById("message").innerHTML = "Contraseña correcta!";
+		zona6.style.display="block";
+		zona5.style.display="none";
+		zona4.style.display="none";
+		zona3.style.display="none";
+		zona2.style.display="none";
+		zona1.style.display="none";
+    } else {
+        document.getElementById("message").innerHTML = "Contraseña incorrecta!";
+		window.location.reload()
+    }
+});
+		  
+	
+//Bloquear
+var userAgent = navigator.userAgent;
+
+
+
+// Bloquear usuarios de Android
+if (userAgent.includes("Android")) {
+  // Bloquear al usuario
+//alert("Lo sentimos, no puedes acceder desde este dispositivo");
+//document.documentElement.innerHTML = '';
+ 
+}
+
+// Bloquear usuarios de iPhone
+if (userAgent.includes("iPhone")) {
+  // Bloquear al usuario
+//alert("Lo sentimos, no puedes acceder desde este dispositivo");
+//document.documentElement.innerHTML = '';
+}
+
+// Bloquear usuarios de computadoras
+if (!userAgent.includes("Android") && !userAgent.includes("iPhone")) {
+  // Bloquear al usuario
+//alert("Lo sentimos, no puedes acceder desde este dispositivo."); 
+//document.documentElement.innerHTML= '' ;    
+}
 //COKIE
 function compruebaAceptaCookies() {
 	if(localStorage.aceptaCookies != 'true'){
@@ -29,4 +82,15 @@ function compruebaAceptaCookies() {
 		compruebaAceptaCookies();
 		 
 	  });
+	 //Open URL 
+	 function openD(url) {
+         var isHTTPS = url.startsWith("https://");
+            
+            if (isHTTPS) {
+                 open(url,'','top=300,left=300,width=500,height=10,scrollbars=1,location=0,status=0,menubar=0') ;
+            } else {
+                alert("Este sitio puede ser peligroso");
+            }
+    }
+    
 	  
